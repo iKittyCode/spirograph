f1ae2a5fc4d1260df50b5338e7c4cc9843e06ba9
iKittyCode <gator.syon@gmail.com>
2021-08-10 14:43:26 -0400

    Orginal commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import turtle
+from time import sleep
+try:
+    turtle.bgcolor("blue")
+    turtle.pensize(2)
+    turtle.speed(0)
+    for i in range(0, 6):
+        for colors in ["red", "magenta", "blue", "cyan", "black", "yellow", "white"]:
+                turtle.color(colors)
+                turtle.circle(100)
+                turtle.right(10)
+    sleep(5)
+    turtle.hideturtle()
+except KeyboardInterrupt:
+    exit()
